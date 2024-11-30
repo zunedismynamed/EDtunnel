1d8ffe7a57dcfd4260ec724a2ea203725d7379ed
zunedismynamed <141910815+zunedismynamed@users.noreply.github.com>
2024-11-30 23:42:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '3c4a3e0e-785f-4b3e-adfd-5ef046040be7';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
 
